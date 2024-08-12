8483fa9a2e9a267f6d479cb14ad48f10d6f4374f
gunjansharma9 <gunjansharmahapur@gmail.com>
2024-08-12 23:11:07 +0530

    login signup chat page completed

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import "./Login.css"
+import assets from '../../assets/assets'
+
+const Login = () => {
+    const [currState,setCurrState] = useState("Sign up");
+  return (
+    <div className='login'>
+        <img src={assets.logo_big} alt="" className='logo' />
+        <form className="login-form">
+            <h2>{currState}</h2>
+            {currState === "Sign up" ? <input type="text" placeholder='username' className="form-input" required /> : ""}
+            <input placeholder='Email address' type="email" className="form-input" required/>
+            <input placeholder='password' type="password" className="form-input" required/>
+            <button type='sumbit'>{currState === "Sign up" ? "Create account" : "Login now"}</button>
+            <div className='login-term'>
+                <input type="checkbox" />
+                <p>Agree to the terms of use & privacy policy.</p>
+            </div>
+            <div className="login-forgot">
+                {
+                    currState === "Sign up" ? 
+                    <p className='login-toggle'>Already have an account? <span onClick={() => setCurrState("Login")}>Login here</span></p> :
+                    <p className='login-toggle'>Create an account <span onClick={() => setCurrState("Sign up")}>click here</span></p>
+                }
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import  "./ProfileUpdate.css"
+import assets from '../../assets/assets'
+
+const ProfileUpdate = () => {
+
+  const [image,setImage] = useState(false);
+  return (
+    <div className='profile'>
+        <div className="profile-container">
+          <form>
+            <h3>Profile Details</h3>
+            <label htmlFor="avatar">
+                <input type="file" id='avatar' accept='.png,.jpg,.jpeg' hidden/>
+                <img src={assets.avatar_icon} alt="" />
+                upload profile image
+            </label>
+            <input type="text" placeholder='Your name' required />
+            <textarea placeholder='Write profile bio' required></textarea>
+            <button type='submit'>Save</button>
+          </form>
+          <img className='profile-pic' src={assets.logo_icon} alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default ProfileUpdate
